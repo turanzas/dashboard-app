af1957896b80965650df34e12d862b832b23abfa
Francisco López <work@loptur.com>
2025-11-25 15:58:01 +0000

    chore: add semantic release configuration and dependencies

---
+++ release.config.js
@@ -0,0 +1,14 @@
+const config = {
+  branches: ['master'],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    ["@semantic-release/git", {
+      "message": "chore(release): update version to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+    }],
+    "@semantic-release/github",
+  ]
+};
+
+module.exports = config;
