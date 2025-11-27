8e26fe129c764f2068bdae0530435e6e6e49f39a
Francisco López <work@loptur.com>
2025-11-27 20:12:44 +0000

    chore: Initialize configuration server project with Gradle setup and basic application structure

---
+++ config-server/README.md
@@ -0,0 +1,5 @@
+# Configuration Server
+
+This configuration server is designed to manage and serve configuration files for various applications. 
+
+It provides a centralized way to store, retrieve, and update configuration settings.
---
+++ config-server/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+	kotlin("jvm") version "2.2.21"
+	kotlin("plugin.spring") version "2.2.21"
+	id("org.springframework.boot") version "4.0.0"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.loptur"
+version = "0.0.1-SNAPSHOT"
+description = "Config Server"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.1.0"
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springframework.cloud:spring-cloud-config-server")
+	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
