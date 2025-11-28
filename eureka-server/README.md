402f12fc916a59f5bdfac5b8b51436a6aff2a685
Francisco López <work@loptur.com>
2025-11-28 18:31:11 +0000

    chore: [eureka-server] Initialize Eureka server project with Gradle setup and basic application structure

---
+++ eureka-server/README.md
@@ -0,0 +1,3 @@
+# Eureka Server
+
+Eureka is a service discovery server that allows microservices to register themselves and discover other services.
---
+++ eureka-server/src/test/kotlin/com/loptur/eurekaserver/EurekaServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.loptur.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
