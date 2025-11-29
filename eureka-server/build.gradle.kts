2c340e6f36cc636c769119b9ec35471571721528
Francisco López <work@loptur.com>
2025-11-29 18:29:49 +0000

    chore: [eureka-server] Update build configuration with Jib plugin and version bump to 1.0.0; add Docker build instructions to README

--- eureka-server/build.gradle.kts
+++ eureka-server/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
 	kotlin("plugin.spring") version "2.2.21"
 	id("org.springframework.boot") version "4.0.0"
 	id("io.spring.dependency-management") version "1.1.7"
+    id("com.google.cloud.tools.jib") version "3.5.1" // docker image generation
 }
 
 group = "com.loptur"
-version = "0.0.1-SNAPSHOT"
+version = "1.0.0"
 description = "Spring Discovery Agent"
 
 java {
@@ -22,10 +23,15 @@ repositories {
 extra["springCloudVersion"] = "2025.1.0"
 
 dependencies {
+    /* KOTLIN */
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    /* SPRING BOOT */
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
+    /* SPRING CLOUD CONFIG CLIENT */
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
+    /* SPRING CLOUD EUREKA CLIENT */
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    /* TESTING */
 	testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -46,3 +52,5 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+jib.to.image = "turanzas/eureka-server:${project.version}"
