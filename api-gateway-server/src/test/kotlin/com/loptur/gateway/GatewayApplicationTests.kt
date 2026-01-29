70bb617e70bae1662edb8c79f1c878991c6937cf
Francisco López <work@loptur.com>
2026-01-29 18:34:43 +0000

    test: update failing test

--- api-gateway-server/src/test/kotlin/com/loptur/gateway/GatewayApplicationTests.kt
+++ api-gateway-server/src/test/kotlin/com/loptur/gateway/GatewayApplicationTests.kt
@@ -2,9 +2,15 @@ package com.loptur.gateway
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+
 
 @SpringBootTest
 class GatewayApplicationTests {
+
+    @MockitoBean
+    val reactiveJwtDecoder: ReactiveJwtDecoder? = null
 
 	@Test
 	fun contextLoads() {
