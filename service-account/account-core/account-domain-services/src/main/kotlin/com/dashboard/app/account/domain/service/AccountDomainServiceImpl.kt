0ad415babcdafca12400d3ebe180f45300aa753b
Francisco López <work@loptur.com>
2026-03-05 07:25:15 +0000

    chore: Add logging library

--- service-account/account-core/account-domain-services/src/main/kotlin/com/dashboard/app/account/domain/service/AccountDomainServiceImpl.kt
+++ service-account/account-core/account-domain-services/src/main/kotlin/com/dashboard/app/account/domain/service/AccountDomainServiceImpl.kt
@@ -7,11 +7,13 @@ import com.dashboard.app.account.domain.model.event.AccountCreatedEvent
 import com.dashboard.app.account.domain.model.event.AccountStatusChangedEvent
 import com.dashboard.app.account.domain.model.exception.AccountDomainException
 import com.dashboard.app.common.domain.model.valueobject.AccountStatus.*
-import com.dashboard.app.common.helper.WithLogging
+import io.github.oshai.kotlinlogging.KotlinLogging
 import lombok.extern.slf4j.Slf4j
 
+private val logger = KotlinLogging.logger {}
+
 @Slf4j
-class AccountDomainServiceImpl: AccountDomainService, WithLogging() {
+class AccountDomainServiceImpl: AccountDomainService {
 
     override fun validateAndInitiateAccount(
         financialEntity: FinancialEntity,
@@ -20,37 +22,39 @@ class AccountDomainServiceImpl: AccountDomainService, WithLogging() {
         validateFinancialEntity(financialEntity)
         validateUser(user)
         val account = Account.initialize(financialEntity.id, user.id)
-        LOG.info { "Account domain entity '${account.id}' created successfully for user ${user.id} under financial entity ${financialEntity.id}." }
+        logger.info { "Account domain entity '${account.id}' created successfully for user ${user.id} under financial entity ${financialEntity.id}." }
         return AccountCreatedEvent(account)
     }
 
     private fun validateFinancialEntity(financialEntity: FinancialEntity) {
         if (!financialEntity.isActive()) {
+            logger.error { "Inactive financial entity ${financialEntity.id} used for account creation"  }
             throw AccountDomainException("Cannot create account for an inactive financial entity.")
         }
     }
 
     private fun validateUser(user: User) {
         if (!user.isActive()) {
+            logger.error { "Inactive user ${user.id} used for account creation"  }
             throw AccountDomainException("Cannot create account for an inactive user.")
         }
     }
 
     override fun activate(account: Account): AccountStatusChangedEvent {
         val updated = account.activate()
-        LOG.info { "Account domain entity '${account.id}' has been activated '${updated}'" }
+        logger.info { "Account domain entity '${account.id}' has been activated '${updated}'" }
         return AccountStatusChangedEvent(account.id, ACTIVE, updated)
     }
 
     override fun deactivate(account: Account): AccountStatusChangedEvent {
         val updated = account.deactivate()
-        LOG.info { "Account domain entity '${account.id}' has been deactivated '${updated}'" }
+        logger.info { "Account domain entity '${account.id}' has been deactivated '${updated}'" }
         return AccountStatusChangedEvent(account.id, INACTIVE, updated)
     }
 
     override fun close(account: Account): AccountStatusChangedEvent {
         val updated = account.close()
-        LOG.info { "Account domain entity '${account.id}' has been closed '${updated}'" }
+        logger.info { "Account domain entity '${account.id}' has been closed '${updated}'" }
         return AccountStatusChangedEvent(account.id, CLOSED, updated)
     }
 
