ae2a942ffd9cac25f064c8e313358f3b81829712
Francisco López <work@loptur.com>
2026-03-10 21:47:15 +0000

    chore: add FinancialEntity and FinancialEntityAddress models with JPA annotations; update POM for database dependencies

---
+++ service-financial-entity/financial-entity-infrastructure/financial-entity-data-access/src/main/kotlin/com/dashboard/app/financial/entity/infrastructure/peristence/entity/FinancialEntityAddress.kt
@@ -0,0 +1,34 @@
+package com.dashboard.app.financial.entity.infrastructure.peristence.entity
+
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import java.util.UUID
+
+@Entity
+@Table(name = "financial_entity_address")
+data class FinancialEntityAddress(
+    @Id val id: UUID,
+    @OneToOne(cascade= [CascadeType.ALL]) @JoinColumn("id")
+    val financialEntity: FinancialEntity,
+    val street: String,
+    val city: String,
+    val state: String,
+    val zipCode: String,
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is FinancialEntityAddress) return false
+        if (id != other.id) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+}
