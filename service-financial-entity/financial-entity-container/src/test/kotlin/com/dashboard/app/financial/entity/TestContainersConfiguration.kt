143582f53111ce911d54b13e0cd8b3ccc4d1f843
Francisco López <work@loptur.com>
2026-03-12 08:52:21 +0000

    chore: implement financial entity messaging with Kafka integration and Avro schema

--- service-financial-entity/financial-entity-container/src/test/kotlin/com/dashboard/app/financial/entity/TestContainersConfiguration.kt
+++ service-financial-entity/financial-entity-container/src/test/kotlin/com/dashboard/app/financial/entity/TestContainersConfiguration.kt
@@ -3,14 +3,14 @@ package com.dashboard.app.financial.entity
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.context.annotation.Bean
-import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.postgresql.PostgreSQLContainer
 
 @TestConfiguration(proxyBeanMethods = false)
 class TestContainersConfiguration {
 
     @Bean
     @ServiceConnection
-    fun postgresContainer(): PostgreSQLContainer<*> =
+    fun postgresContainer(): PostgreSQLContainer =
         PostgreSQLContainer("postgres:latest")
             .withInitScripts("init-schema.sql", "init-data.sql")
 
